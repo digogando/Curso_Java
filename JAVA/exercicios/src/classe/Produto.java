b8f2a89265c884692649e0c885a03e4fdd267d0c
RODRIGO OGANDO FERNANDES <71289621+digogando@users.noreply.github.com>
2021-09-06 20:24:13 -0300

    Refatoração do Codigo

--- JAVA/exercicios/src/classe/Produto.java
+++ JAVA/exercicios/src/classe/Produto.java
@@ -6,7 +6,26 @@ public class Produto {
 	double preco;
 	double desconto;
 	
+	Produto(){
+		
+	}
+	
+	Produto(String nomeInicial, double precoInicial,
+			double descontoInicial){
+		nome = nomeInicial;
+		preco = precoInicial;
+		desconto = descontoInicial;
+	}
+	
+	//Produto(){
+		
+	//}
+	
 	double precoComDesconto (){
 		return preco * (1 - desconto);
+	}
+	
+	double precoComDesconto(double descontoGerente){
+		return preco * (1 - desconto + descontoGerente);
 	}
 }
