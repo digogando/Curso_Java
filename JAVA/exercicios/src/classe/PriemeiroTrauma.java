7e754af43775e4f77e579ecdff75c76a32d9e405
RODRIGO OGANDO FERNANDES <71289621+digogando@users.noreply.github.com>
2021-09-07 17:13:53 -0300

    Upload Desafio

---
+++ JAVA/exercicios/src/classe/PriemeiroTrauma.java
@@ -0,0 +1,15 @@
+package classe;
+
+public class PriemeiroTrauma {
+	
+	int a = 3; //Não pode mexer nessa linha
+
+	public static void main(String[] args) {
+		
+		//Pode mexer aqui
+		PriemeiroTrauma p = new PriemeiroTrauma();
+		System.out.println(p.a);
+
+		//Pode mexer aqui
+	}
+}
