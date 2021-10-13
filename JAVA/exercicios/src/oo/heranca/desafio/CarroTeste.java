3a9829db823bb7b3af3aaa9f8663456cd5a5b298
RODRIGO OGANDO FERNANDES <71289621+digogando@users.noreply.github.com>
2021-10-13 14:35:45 -0300

    Upload exerc

---
+++ JAVA/exercicios/src/oo/heranca/desafio/CarroTeste.java
@@ -0,0 +1,32 @@
+package oo.heranca.desafio;
+
+public class CarroTeste {
+
+	public static void main(String[] args) {
+		
+		Chevette c1 = new Chevette();
+		
+		c1.pisaAcelerador();
+		System.out.println(c1);
+		
+		c1.pisaAcelerador();
+		System.out.println(c1);
+		
+		c1.pisaAcelerador();
+		System.out.println(c1);
+
+		Uno c2 = new Uno();
+		
+		c2.pisaAcelerador();
+		System.out.println(c2);
+		
+		c2.pisaAcelerador();
+		System.out.println(c2);
+		
+		c2.pisaAcelerador();
+		System.out.println(c2);
+		
+		
+	}
+
+}
