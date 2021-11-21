26a3e48bfdec20dc02e2c4b85a3626aad62608ec
RODRIGO OGANDO FERNANDES <71289621+digogando@users.noreply.github.com>
2021-11-21 10:13:14 -0300

    Add files via upload

---
+++ JAVA/exercicios/src/padroes/observer/Porteiro.java
@@ -0,0 +1,48 @@
+package padroes.observer;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+public class Porteiro {
+
+	private List<ObservadorChegadaAniversariante> observadores
+		= new ArrayList<>();
+	
+	public void registrarObservador(
+			ObservadorChegadaAniversariante observador){
+		observadores.add(observador);
+	}
+	
+	public void monitorar(){
+		@SuppressWarnings("resource")
+		Scanner entrada = new Scanner(System.in);
+		
+		String valor = "";
+		
+		while(!"sair".equalsIgnoreCase(valor)){
+			
+			System.out.print("Aniversariante chegou? ");
+			valor = entrada.nextLine();
+			
+			if("sim".equalsIgnoreCase(valor)){
+				// Criando o evento
+				EventoChegadaAniversariante evento =
+						new EventoChegadaAniversariante(new Date());
+						
+				//Notificando os observadores
+				observadores.stream()
+					.forEach(o -> o.chegou(evento));
+				valor = "sair";
+			} else {
+				System.out.println("Alerme falso.");
+			}
+		}
+	}
+
+	public void registrarObservador(Namorada namorada) {
+		// TODO Auto-generated method stub
+		
+	}
+}
