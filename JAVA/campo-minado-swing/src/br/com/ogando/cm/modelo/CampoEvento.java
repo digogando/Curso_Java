de9bba77721e3c47c230450a0e6d49e0124e03f2
RODRIGO OGANDO FERNANDES <71289621+digogando@users.noreply.github.com>
2021-11-26 21:07:05 -0300

    Upload Campo Minado Swing

---
+++ JAVA/campo-minado-swing/src/br/com/ogando/cm/modelo/CampoEvento.java
@@ -0,0 +1,6 @@
+package br.com.ogando.cm.modelo;
+
+public enum CampoEvento {
+
+	ABRIR, MARCAR, DESMARCAR, EXPLODIR, REINICIAR
+}
---
+++ JAVA/campo-minado-swing/src/br/com/ogando/cm/modelo/CampoObservador.java
@@ -0,0 +1,6 @@
+package br.com.ogando.cm.modelo;
+
+public interface CampoObservador {
+
+	public void eventoOcorreu(Campo campo, CampoEvento evento);
+}
